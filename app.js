f5bb970e2ce61a2950edc133f18f45992a3f4c88
angadsharma1016 <angadsharma1016@gmail.com>
2019-03-24 10:55:19 +0530

    dockerizing

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("."))
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.listen("8080", ()=>console.log("listening..."))
